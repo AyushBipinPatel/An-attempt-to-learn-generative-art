41f908266ed88c8d5a493e0bdc42d78deca4afdc
AyushBipinPatel <ayush.ap58@gmail.com>
2021-10-15 12:59:08 +0530

    check git from R

--- First Attempt.R
+++ First Attempt.R
@@ -1,31 +1,31 @@
 ## Attempting to learn generative art
 
-library(tidyverse)
-library(gganimate)
-
-tibble(
-  rads = c(0:3600)*(pi/180),
-  rads2 = c(3600:0)*(pi/180),
-  sinof = sin(rads),
-  cosof = cos(rads),
-  tanof = tan(rads)
-) %>% 
-  ggplot()+
-  coord_polar()+
-  theme(
-    panel.background = element_blank(),
-    axis.title = element_blank(),
-    axis.text = element_blank(),
-    axis.ticks = element_blank()
-  ) +
-  geom_line(aes(rads,sinof),
-            colour = "#2bc846", 
-            size = 1)+
-  transition_reveal(along = rads)+
-  geom_line(aes(rads2,cosof),
-            colour = "#ff010d", 
-            size = 1)+
-  transition_reveal(along = rads2) -> animated_chart
+  library(tidyverse)
+  library(gganimate)
+  
+  tibble(
+    rads = c(0:3600)*(pi/180),
+    rads2 = c(3600:0)*(pi/180),
+    sinof = sin(rads),
+    cosof = cos(rads),
+    tanof = tan(rads)
+  ) %>% 
+    ggplot()+
+    coord_polar()+
+    theme(
+      panel.background = element_blank(),
+      axis.title = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank()
+    ) +
+    geom_line(aes(rads,sinof),
+              colour = "#2bc846", 
+              size = 1)+
+    transition_reveal(along = rads)+
+    geom_line(aes(rads2,cosof),
+              colour = "#ff010d", 
+              size = 1)+
+    transition_reveal(along = rads2) -> animated_chart
 
 
 
